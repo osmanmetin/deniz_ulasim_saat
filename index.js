ac5ae205d0791264d0a4607e5548eed626a1cf0d
osmanmetin <osmanmetina@gmail.com>
2021-01-17 21:34:04 +0300

    ABEY

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ let vapur_saat = document.querySelector(".vapur_saat");
 let yakin_saat = document.querySelector(".yakin_saat");
 let sefer_goster = document.querySelector(".sefer_goster");
 let enyakin_saat = document.querySelector(".enyakin_saat");
-let iskele = document.querySelector("#iskele");
+let konum = document.querySelector("#iskele");
 let tablo = document.querySelector(".tablo");
-let thead_success =document.querySelector(".thead-success ");
+let bilgilendirme = document.querySelector(".bilgilendirme");
+let thead_success = document.querySelector(".thead-success ");
 
 addListener();
 
@@ -37,19 +38,21 @@ function show() {
 
 show();
 
-let arr = ["6,0", "6,15", "6,30"];
+let arr = ["6,0", "6,15", "6,30"]; // Beşiktaştan yola çıkan her üç vapurun başlangıç saatleri
 
 // Sefer Saatleri
 function dondur() {
   for (var i = 0; i < 11; i++) {
     arr1 = arr.slice(arr.length - 3, arr.length);
     arr1 = kadikoy(arr1);
-    arr2 = arr.slice(arr1.length - 3, arr1.length);
+    arr2 = arr1.slice(arr1.length - 3, arr1.length);
     arr2 = besiktas(arr2);
   }
-  return arr;
+  arr = ["6,0", "6,15", "6,30"];
+  return arr2;
 }
 
+// Kadıköy serferleri hesaplama
 function kadikoy(saat) {
   saat.forEach(function (item) {
     item = item.split(",");
@@ -70,6 +73,7 @@ function kadikoy(saat) {
   return arr;
 }
 
+// Beşiktaş seferleri hesaplama
 function besiktas(saat) {
   saat.forEach(function (item) {
     item = item.split(",");
@@ -93,7 +97,7 @@ function besiktas(saat) {
 // Sefer saatleri gösteren buton event
 
 function vapursaat() {
-  thead_success.setAttribute("class","thead-success d-inlineblock")
+  thead_success.setAttribute("class", "thead-success d-inlineblock");
   items = dondur();
   html = " ";
   iskele = ["Beşiktaş", "Kadıköy"];
@@ -125,29 +129,26 @@ function vapursaat() {
 
 //En yakın seferin bulınması
 function hesap(saat_dizi) {
-  
-  let array=[];
-  let array1=[];
+  let array = [];
+  let array1 = [];
   let date = new Date();
   date = date.toString().split(" ");
   date = date[4].slice(0, 5);
   date = date.replace(":", ",");
-  for (var i = 0; i < saat_dizi.length; i=i+3) {
+  for (var i = 0; i < saat_dizi.length; i = i + 3) {
     if (i % 2 == 0) {
       array.push(saat_dizi[i]);
-      array.push(saat_dizi[i+1]);
-      array.push(saat_dizi[i+2]);
+      array.push(saat_dizi[i + 1]);
+      array.push(saat_dizi[i + 2]);
     } else {
       array1.push(saat_dizi[i]);
-      array1.push(saat_dizi[i+1]);
-      array1.push(saat_dizi[i+2]);
+      array1.push(saat_dizi[i + 1]);
+      array1.push(saat_dizi[i + 2]);
     }
   }
 
-  
-  
-  if(iskele.value==="1"){
-   
+  // Hangi İskelede ise o bilgiyi alıyoruz
+  if (konum.value === "1") {
     array.unshift(date);
     saat_dizi1 = array.sort();
     index = saat_dizi1.indexOf(date);
@@ -156,43 +157,34 @@ function hesap(saat_dizi) {
     } else {
       return saat_dizi1[index + 1];
     }
-
-  }else if(iskele.value=="2"){
-  
+  } else if (konum.value == "2") {
     array1.unshift(date);
-
     saat_dizi2 = array1.sort();
-    console.log(array1);
     index = saat_dizi2.indexOf(date);
     if (index === 0) {
       return false;
     } else {
       return saat_dizi2[index + 1];
     }
-
+  } else {
+    return "iskele seç";
   }
- 
 }
 
-//İşten çıkış yaptım butonu
+//En Yakın Sefer
 
 function istencikis() {
   a = dondur();
-  hesap = hesap(a);
+  deger = hesap(a);
 
-  if (hesap === false) {
-    div = document.createElement("div");
-    div.setAttribute("class", "text-center border rounded bg-danger mt-3 p-2");
-    hesap = `Bugünlük sefer bitti `;
-    div.appendChild(document.createTextNode(hesap));
-    yakin_saat.appendChild(div);
+  if (deger === false) {
+    aciklama = `Bugünlük sefer bitti `;
+    html = "";
+    html += `<div class="text-center mt-3 bg-danger border rounded p-3"> ${deger} </div>`;
+    bilgilendirme.innerHTML = html;
   } else {
-    div = document.createElement("div");
-    div.setAttribute("class", "text-center border rounded bg-warning mt-3 p-2");
-    sonuc = `En Yakın Sefer Saat =  ${hesap}`;
-    div.appendChild(document.createTextNode(sonuc));
-    yakin_saat.appendChild(div);
-
-    
+    html = "";
+    html += `<div class="text-center mt-3 bg-success border rounded p-3"> En Yakın Sefer Saat :${deger} </div>`;
+    bilgilendirme.innerHTML = html;
   }
 }
